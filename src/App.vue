0446a23185b1d426a610ba3fe022e84dc8be6d93
pavelborisenko <p.borisenko@jetmail.cc>
2020-10-30 10:12:39 +0300

    Выполнил тз

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <fruits />
+  </div>
+</template>
+
+<script>
+import Fruits from './components/Fruits.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Fruits
+  }
+}
+</script>
+
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+  * {
+    box-sizing: border-box;
+  }
+  html, body  {
+    height: 100%;
+  }
+  body {
+    background: #79C578;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: Roboto;
+  }
+</style>
---
+++ src/components/Fruits.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="fruits">
+    <div class="fruits-title">{{currentFruit}}</div>
+    <input maxlength="2" class="fruits-input" v-model="current" @input="onInput"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      current: 1,
+      fruits: ['яблоко', 'груша', 'банан', 'киви', 'ананас', 'папайя', 'мандарин', 'апельсин', 'айва', 'лимон']
+    }
+  },
+  computed: {
+    currentFruit() {
+      return this.fruits[this.current - 1]
+    }
+  },
+  methods: {
+    onInput({target}) {
+      const
+          val = target.value,
+          twoSymbols = +val.slice(-2),
+          newVal = +twoSymbols < 11 && +twoSymbols > 0 ? +val : (+val.slice(-1) || 1)
+      if (val !== newVal) {
+        target.value = newVal;
+        target.dispatchEvent(new Event('input'));
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .fruits {
+    width: 388px;
+    height: 254px;
+    background-color: rgba(255, 255, 255, 0.42);
+    border-radius: 8px;
+    font-size: 36px;
+    text-align: center;
+    padding: 44px 30px;
+  }
+  .fruits-title {
+    text-transform: capitalize;
+    margin-bottom: 16px;
+    height: 77px;
+  }
+  .fruits-input {
+    font-size: 36px;
+    line-height: 42px;
+    width: 124px;
+    outline: none;
+    border: none;
+    text-align: center;
+  }
+</style>
